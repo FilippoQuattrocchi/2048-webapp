e3f7e4ffc78ade680e11c08fb5ce9a88d0e5b779
Filippo Quattrocchi <filippo.quattrocchi23@outlook.it>
2024-08-14 19:46:03 +0900

    Feat: Crud operations for score

--- express-backend/src/models/score.model.ts
+++ express-backend/src/models/score.model.ts
@@ -1,13 +1,35 @@
 import { prisma } from "../server";
 
-const createScore = async (req: Request, res: Response) => {};
+const createScore = async (userId: number,value: number) => {
+	return await prisma.scoreboard.create({
+		data: {
+			value,
+            userId
+		},
+	});
+};
 
 const selectAllScore = async () => {
 	return await prisma.scoreboard.findMany();
 };
-const getScore = async (req: Request, res: Response) => {};
-const updateScore = async (req: Request, res: Response) => {};
-const deleteScore = async (req: Request, res: Response) => {};
+const getScore = async (id: number) => {
+    return await prisma.scoreboard.findUnique({
+        where: { id}
+    });
+};
+const updateScore = async (id: number, value: number) => {
+    return await prisma.scoreboard.update({
+        where: {id},
+        data: {
+            value
+        },
+    })
+};
+const deleteScore = async (id: number) => {
+    return await prisma.scoreboard.delete({
+			where: { id }
+		});
+};
 
 export default {
 	createScore,
